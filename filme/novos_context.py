9c8e0adf1ceeea52765f8b1b142037222eceb32c
MarceloFantin <marcelo_fantin@yahoo.com.br>
2026-06-10 20:40:54 -0300

    aleração do homefilmes

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -5,10 +5,15 @@
 from .models import Filme
 
 def lista_filmes_recentes(request):
-    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:10]
-    return {"lista_filmes_recentes": lista_filmes}
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:8]
+    #esse filme destaque pode ser com outros tipos de filtro 
+    filme_destaque = Filme.objects.all().order_by('-data_criacao')[0:1]
+    return {"lista_filmes_recentes": lista_filmes, "filme_destaque": filme_destaque}
 
 def lista_filmes_emalta(request):
-    lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:10]
+    lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:8]
     return {"lista_filmes_emalta": lista_filmes}
 
+def filmes_destaque(request):
+    filme_destaque = Filme.objects.order_by('-data_criacao')[0:1]
+    return {"filme_destaque": filme_destaque}
